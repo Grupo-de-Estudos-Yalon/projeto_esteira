71f21376eaf945824c9c59a82ffea66916ab5f57
jvsajv <runner.troll.rt@gmail.com>
2023-09-08 05:04:23 -0300

    feat: Altera para que o token seja renovado apenas quando necessario

--- src/utils/spotifyUtils.js
+++ src/utils/spotifyUtils.js
@@ -2,30 +2,49 @@
 const axios = require('axios').default;
 require('dotenv').config()
 
+var token = {
+    bearer: "",
+    expires: ""
+}
+
+function tokenExpired(){
+    return token.expires < new Date()
+}
+
 async function authenticate() {
-    const data = `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
-    try {
-        const response = await axios.post(`https://accounts.spotify.com/api/token`, data,
-            {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
+    if (tokenExpired()) {
+        const data = `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+        try {
+            const response = await axios.post(`https://accounts.spotify.com/api/token`, data,
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
                 }
-            }
-        );
-        return response.data;
-    } catch (error) {
-        console.error(error);
+            );
+            token.bearer = response.data.access_token
+            expires = new Date()            
+            expires_in = response.data.expires_in * 10
+            expires.setTime(expires.getTime() + expires_in)
+            token.expires = expires
+            return token.bearer;
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        return token.bearer
     }
 }
 
-async function getGenres(){
+async function getGenres() {
     try {
-        const token = await authenticate();
+
+        await authenticate();
         const response = await axios.get(`https://api.spotify.com/v1/recommendations/available-genre-seeds`,
             {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': `Bearer ${token.access_token}`
+                    'Authorization': `Bearer ${token.bearer}`
                 }
             }
         );
